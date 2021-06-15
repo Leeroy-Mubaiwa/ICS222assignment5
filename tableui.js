9f498a68df72a7c445632f26488a5deb87c797f7
Leeroy-Mubaiwa <markmubaiwa@gmail.com>
2021-06-15 05:13:49 +0200

    First Attempt at solving

---
+++ tableui.js
@@ -0,0 +1,20 @@
+function doFunction() {
+    var els = document.getElementsByTagName("tr");
+
+    for (var i = 0; i < els.length; i += 2) {
+        els[i].style = "background-color:black; color: white"
+    }
+
+
+}
+
+function myFunction() {
+    var element, name, arr;
+    // element = document.getElementById("myDIV");
+    element = document.getElementsByClassName("tables")
+    name = "zebra";
+    arr = element.className.split(" ");
+    if (arr.indexOf(name) == -1) {
+        element.className += " " + name;
+    }
+}
