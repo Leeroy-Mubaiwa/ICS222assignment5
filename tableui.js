0fa44e1c66b254df4be4ab7a3d31665f0cf7dc7a
Leeroy-Mubaiwa <markmubaiwa@gmail.com>
2021-06-15 05:15:42 +0200

    Adding more functions

--- tableui.js
+++ tableui.js
@@ -18,3 +18,8 @@ function myFunction() {
         element.className += " " + name;
     }
 }
+
+function myFunction2() {
+    var element = document.getElementsByClassName("tables");
+    element.classList.add("zebra");
+}
