40ab38da41cdfb2eabd9d89bfe367d16976d1072
Leeroy-Mubaiwa <markmubaiwa@gmail.com>
2021-06-17 15:38:11 +0200

    Mobile first

--- tableui.js
+++ tableui.js
@@ -1,19 +1,18 @@
 function doFunction() {
-    var els = document.getElementsByTagName("tr");
-    for (var i = 1; i < els.length; i += 2) {
-        els[i].style = "background-color:black; color: white"
-    }
+    let odds = document.querySelectorAll('table tr:nth-child(odd)');
+    odds.forEach((odds) => {
+        //adds a class made in css. This class overides the set class of colors and picks this one
+        odds.classList.add('zebra');
+    });
 
 
 }
 
-function myFunction() {
-    var element, name, arr;
-    // element = document.getElementById("myDIV");
-    element = document.getElementsByClassName("tables")
-    name = "zebra";
-    arr = element.className.split(" ");
-    if (arr.indexOf(name) == -1) {
-        element.className += " " + name;
-    }
+function undoFunction() {
+    let odds = document.querySelectorAll('table tr:nth-child(odd)');
+    odds.forEach((odds) => {
+
+        ///this code removes the recent class we aaded
+        odds.classList.remove('zebra');
+    });
 }
