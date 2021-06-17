71ba3ba191a96c5ed2a50428006ed796eb6cb2ac
Leeroy-Mubaiwa <markmubaiwa@gmail.com>
2021-06-17 14:59:37 +0200

    Button and Javascript additions

--- tableui.js
+++ tableui.js
@@ -1,6 +1,5 @@
 function doFunction() {
     var els = document.getElementsByTagName("tr");
-
     for (var i = 1; i < els.length; i += 2) {
         els[i].style = "background-color:black; color: white"
     }
